c77cdbfe13aa1656e9003a77e968fc9f5c8cf87a
Alexandre R. Saccol <arsaccol@gmail.com>
2018-06-21 03:43:25 -0300

    Please don't try to make a lot of sense of main yet, I've changed it a lot since it last made sense. It's just included to demonstrate how one could use the trie.

--- main.py
+++ main.py
@@ -1,6 +1,47 @@
+import sys
+from pprint import pprint
+from collections import namedtuple
+
+from src.Trie import Trie
+
+
+Review = namedtuple('Review', ['score', 'text'])
+
 
 def main():
-    print('hello')
+    if len(sys.argv) != 2:
+        sys.exit('Wrong arguments')
+
+    input_file = open(sys.argv[1])
+
+    # create list of reviews associated with scores
+    review_list= []
+    for line in input_file:
+        review_list.append( Review(score=line[0], text=line[1:]) )
+
+    #word_dictionary = dict() # word -> number of occurrences
+    word_dictionary = Trie()
+    
+    for review in review_list:
+        review = Review(review.score, review.text.split(' '))
+        current_review_dict = dict()
+        for word in review.text:
+            word = word.replace('\t', '')
+            word = word.replace('\n', '')
+            word = word.lower()
+            '''
+            if word not in current_review_dict:
+                current_review_dict[word] = []
+            else:
+                current_review_dict[word].append(review.score)
+            '''
+            word_dictionary.insert(word)
+
+    #print(word_dictionary)
+    #print('combination: ', ['combination'])
+    print(word_dictionary.get_all_words())
+
+    
 
 if __name__ == '__main__':
     main()
