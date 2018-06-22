a6b4e1e6689317f4b45cb9d2c6ec5da0724f60ac
gcsimon <gio.simon@hotmail.com>
2018-06-22 15:20:48 -0300

    added a few coments for future algorithm coding

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import sys
 from pprint import pprint
 from collections import namedtuple
-
 from src.Trie import Trie
 
-
 Review = namedtuple('Review', ['score', 'text'])
-
 
 def main():
    # if len(sys.argv) != 2:
@@ -35,21 +32,29 @@ def main():
             word = word.replace('\t', '')
             word = word.replace('\n', '')
             word = word.lower()
+            word = word.replace('.', '')
+            word = word.replace(',', '')
             '''
             if word not in current_review_dict:
                 current_review_dict[word] = []
             else:
                 current_review_dict[word].append(review.score)
             '''
-            #word_dictionary.insert(word)
-            word_dictionary[word] = None
+            if word in word_dictionary:
+                #add 1 in number of ocurrencies
+                print(word)
+            else:
+                word_dictionary.insert(word, review.score)
+                #sets word value
+                #sets number of ocurrencies to 1
+
+            #word_dictionary[word] = None
 
     #print(word_dictionary)
     #print('combination: ', ['combination'])
-    print(word_dictionary.get_all_words())
+    #print(word_dictionary.get_all_words())
     print('Trie dictionary has',len(word_dictionary),'words')
 
-    
 
 if __name__ == '__main__':
     main()
