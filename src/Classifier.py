26fa64f27ca94765a43ab381ee83c66e94adc893
Alexandre R. Saccol <arsaccol@gmail.com>
2018-06-24 05:31:58 -0300

    Add some docs to Classifier. No Kaggle format functionality implemented yet.

--- src/Classifier.py
+++ src/Classifier.py
@@ -8,15 +8,21 @@ Review = namedtuple('Review', ['score', 'text'])
 WordStats = namedtuple('WordStats', ['score_sum', 'occurrences', 'average_score'])
 
 class Classifier:
+    # TODO: implement Kaggle test functionality
+    '''
+    Initialize with a filename, it'll extract scores for the words from sentences in 
+        the input file.
+    Then, call Classifier.evaluate_sentence at your leisure to evaluate sentences.
+    '''
 
-    def __init__(self, filename='./input/single-words.txt'):
+    def __init__(self, filename='./input/movieReviews.txt'):
         self.word_stats_trie= Trie()
         self.review_list = self.get_review_list(filename)
         self.set_word_stats()
 
     def evaluate_sentence(self, sentence, store_absent=False):
         '''
-        Calculates average of scores of all words in the sentence.
+        Calculates and returns average of scores of all words in the sentence.
         '''
         
         total_sum = 0.0
@@ -39,6 +45,21 @@ class Classifier:
         
 
     def set_word_stats(self):
+        '''
+        Finds WordStats for every word in each review, and stores them
+            in self.word_stats_trie.
+        Requires self.get_review_list() to have been run.
+
+        For each word we may define a WordStats.
+
+        -WordStats.score_sum is the sum of average_scores of each occurrence of
+            each our given word processed so far.
+
+        -WordStats.occurrences is the number of times the word has been found.
+
+        -WordStats.average_score is simply the average, score_sum divided by occurrences,
+            of our given word.
+        '''
         for review in self.review_list:
             for word in review.text.split():
                 if word in self.word_stats_trie:
@@ -62,6 +83,10 @@ class Classifier:
 
 
     def get_review_list(self, filename):
+        '''
+        Gets a list of Reviews.
+        Bear in mind that Review.score is an integer.
+        '''
         try:
             review_file = open(filename, encoding='UTF-8-sig')
         except IOError:
@@ -79,6 +104,15 @@ class Classifier:
 
 
     def get_review_from_line(self, line):
+        '''
+        Gets reviews from lines of text from the input file.
+
+        -Review.score is stored as an integer, the first "word" in the line.
+
+        -Review.text is stored as a single string containing the review text proper.
+
+        '''
+        
         score = int(line.split()[0])
         text = self.clean_word(line[1:])
 
@@ -86,6 +120,9 @@ class Classifier:
         
             
     def clean_word(self, word):
+        '''
+        Removes unwanted characters.
+        '''
         word = word.replace('\t', '')
         word = word.replace('\n', '')
         word = word.lower()
@@ -98,6 +135,7 @@ if __name__ == '__main__':
 
     import sys
     if len(sys.argv) == 2:
+        # command-line arguments to avoid typing stuff over again and again
         classifier = Classifier(sys.argv[1])
 
     else:
